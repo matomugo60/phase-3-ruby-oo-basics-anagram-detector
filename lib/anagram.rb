8a66bac202208d4c33bc54c60255e3a564684b88
matomugo60 <matomugo60@gmail.com>
2023-02-08 15:36:33 +0300

    Complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+
+class Anagram
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(word_list)
+      word_list.select { |w| w.downcase.chars.sort == @word.chars.sort && w.downcase != @word }
+    end
+  end
+  
